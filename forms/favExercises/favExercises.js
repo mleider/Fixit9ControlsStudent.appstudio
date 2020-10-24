35a73f8febe7f2ad327209f5b1d6b2de5e3dbd71
mleider <mckenzieleider@creighton.edu>
2020-10-24 10:47:40 -0500

    favExercises changes

--- forms/favExercises/favExercises.js
+++ forms/favExercises/favExercises.js
@@ -1,20 +1,22 @@
-var exercises = ["pullups","pushups","situps","jogging","plank","curls"]
-var coreExercises = ["situps","plank"]
+var exercises = ["pullups", "pushups", "situps", "jogging", "plank", "curls"]
+var coreExercises = ["situps", "plank"]
 
-favExercises.onshow=function(){
-for (i=0;i<exercises.length;i++){
-selExercises.addItem(exercises[i])
+favExercises.onshow = function() {
+  for (i = 0; i < exercises.length; i++) {
+    selExercises.addItem(exercises[i])
+  }
+
 }
-for (i=0;i<coreExercises.length;i++){
-selExercises.addItem(coreExercises[i])
+
+selExercises.onfocusout = function() {}
+
+btnSubmit.onclick = function() {
+  if (selExercises.text[0] == "situps" && selExercises.text[1] == "plank") {
+    lblExercises.value = `You chose ${selExercises.text[0]} and ${selExercises.text[1]} - those are the two core exercises.`
+  } else {
+    lblExercises.value = "You did not pick the two core exercises"
+  }
 }
-}
-btnMobile.onclick=function(){
-ChangeForm(mobileNav)
-}
-selExercises.onfocusout=function(){
-let message = "You chose:" 
-for (i = 0; i <= selExercises.text.length - 1; i++)
-message = message + " "+ selExercises.text[i]
-lblExercises.value = message
+btnMobile.onclick = function() {
+  ChangeForm(mobileNav)
 }
